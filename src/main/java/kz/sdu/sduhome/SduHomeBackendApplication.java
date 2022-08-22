46864e48d4a8a50623bf1e911bea172be8c94ce0
Sanzhar Zhanibekov <semid46@gmail.com>
2022-08-22 23:35:24 +0600

    init

---
+++ src/main/java/kz/sdu/sduhome/SduHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.sdu.sduhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SduHomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SduHomeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/sdu/sduhome/SduHomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sdu.sduhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SduHomeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
